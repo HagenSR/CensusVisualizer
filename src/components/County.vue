d262eedb70146f2cb4e2312dd6f0102783b3153b
Sean Hagen <sean.r.hagen@gmail.com>
2020-05-31 11:10:40 -0500

    Began work on google function interaction

---
+++ src/components/County.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1></h1>
+</template>
+
+<script>
+import getByID from "./../serverTwo.js"
+export default {
+    name: "County",
+    data: () => ({
+        info: null
+    }),
+    created: () => {
+        getByID(1001).then(e => console.log(e))
+    }
+}
+</script>
---
+++ src/functionsInteraction.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+
+async function getCounty(count) {
+  var url = 'https://us-central1-oval-proxy-276213.cloudfunctions.net/getCounty';
+  return new Promise(function (res, fail) {
+    axios.post(url, {
+      county: count,
+    })
+      .then(function (response) {
+        res(response.data);
+      })
+      .catch(function (error) {
+        fail(error);
+      });
+  })
+
+}
+
+async function getByID(id) {
+  var url = 'https://us-central1-oval-proxy-276213.cloudfunctions.net/getCountByID';
+  return new Promise(function (res, flnk) {
+    axios.post(url, {
+      ID: id,
+    })
+      .then(function (response) {
+        res(response.data);
+      })
+      .catch(function (error) {
+        flnk(error);
+      });
+  })
+}
+
+exports.getByID = getByID();
+exports.getCounty = getCounty();
+
+
+
